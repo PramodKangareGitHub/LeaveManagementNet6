87c2562f7f9739b5e8dd02bfc0d359eb5524c303
PK00688112 <pk00688112@techmahindra.com>
2022-06-03 15:27:33 +0530

    LeaveType changes

--- LeaveManagement.Web/Models/LeaveTypeVM.cs
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -5,9 +5,13 @@ namespace LeaveManagement.Web.Models
     public class LeaveTypeVM
     {
         public int Id { get; set; }
+        [Display(Name = "Leave Type")]
+        [Required(ErrorMessage ="Leave type is required")]
         public string Name { get; set; }
 
         [Display(Name="Default no of Days")]
+        [Required(ErrorMessage = "No of Days required")]
+        [Range(1, 25, ErrorMessage ="Please enter valid number")]
         public int DefaultDays { get; set; }
     }
 }
